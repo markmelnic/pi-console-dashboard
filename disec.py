cb21f97a9e72bb906d51904b50aa9bc802a2eea1
Mark Melnic <markhappy200@gmail.com>
2021-01-12 20:13:49 +0200

    Multiple improvements and projects display added

--- disec.py
+++ disec.py
@@ -1,5 +1,6 @@
 import os, wifisec, platform
 from speedtest import SpeedTest
+from hku import fetch_heroku
 from rich import print
 from rich.table import Table
 
@@ -12,30 +13,43 @@ def display():
         clear = lambda: os.system('cls')
 
     while True:
-        devices = wifisec.who()
+        main_tab = Table(header_style="bold magenta", show_edge=False, show_lines=False, show_header=False)
+        main_tab.add_column("Network")
+        main_tab.add_column("Projects")
+
+        projects_tab = Table(header_style="bold magenta", title="Projects")
+        projects_tab.add_column("Name")
+        projects_tab.add_column("Running")
+        for p in fetch_heroku():
+            if p[1]:
+                projects_tab.add_row(str(p[0]), "[green]True[/green]")
+            else:
+                projects_tab.add_row(str(p[0]), "[red]False[/red]")
 
         try:
             st = SpeedTest()
-            ping = st.ping()
-            ping = "%.2f" % ping
+            ping = "%.2f" % st.ping()
+            #download = "%.2f" % st.download()
+            #upload = "%.2f" % st.upload()
         except OSError:
             ping = "# Connection Error"
+        status_tab = Table(title="Network Status", header_style="bold magenta")
+        status_tab.add_column("Stat")
+        status_tab.add_column("Data")
+        status_tab.add_row("Ping", str(ping))
+        #status_tab.add_row("Download", str(download))
+        #status_tab.add_row("Upload", str(upload))
 
-        table = Table(show_header=True, header_style="bold magenta")
-        table.add_column("IP")
-        table.add_column("MAC")
-        table.add_column("NAME")
+        devices = wifisec.who()
+
+        connected_tab = Table(title="Connected Devices", header_style="bold magenta")
+        connected_tab.add_column("NAME")
+        connected_tab.add_column("MAC")
+        connected_tab.add_column("IP")
 
         clear()
 
-        print("[bold cyan]Network stats:[/bold cyan]")
-        print(" "*4, "Ping: [green]%s[/green]ms" % ping)
-        #print(" "*4,st.download())
-        #print(" "*4,st.upload())
-
-        print("\n[bold cyan]Connected devices:[/bold cyan]")
         for d in devices:
-
             ip = d[1].split(".")
             for i, sec in enumerate(ip[1:-1]):
                 ip[i+1] = "#"*len(sec)
@@ -50,7 +64,8 @@ def display():
             if "(" in name:
                 name = name[:name.find("(")]
 
-            table.add_row(ip, mac, name)
+            connected_tab.add_row(name, mac, ip)
 
-        print(table)
+        main_tab.add_row(status_tab, connected_tab, projects_tab)
+        print(main_tab)
         #print("[bold red]No device secured, breach possible.[/bold red]")
